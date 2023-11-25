6df26ae4bc24eb158fe6b1ecf6bef181889fdb11
murat@mediasupreme.com <murat@mediasupreme.com>
2023-11-25 20:18:32 +0000

    Added spectre console.

--- 15Puzzle.CLI/Program.cs
+++ 15Puzzle.CLI/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Spectre.Console;
 Console.WriteLine("Hello, World!");
+
+var table = new Table();
+table.AddColumn(new TableColumn("[u]Foo[/]"));
+table.AddColumn(new TableColumn("[u]Bar[/]"));
+table.AddColumn(new TableColumn("[u]Baz[/]"));
+
+// Add some rows
+table.AddRow("Hello", "[red]World![/]", "");
+table.AddRow("[blue]Bonjour[/]", "[white]le[/]", "[red]monde![/]");
+table.AddRow("[blue]Hej[/]", "[yellow]Världen![/]", "");
+
+AnsiConsole.Write(table);
